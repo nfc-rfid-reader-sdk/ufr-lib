db96acfe68219a53a534e060b1f39d7808aeefd6
developer4 <support@d-logic.rs>
2017-08-15 12:41:41 +0200

    uFR library version 4.0.30
    BugFix in function APDUTransceiveHnd():
    -In some situations there was an error calculating Le byte.

--- include/uFCoder.h
+++ include/uFCoder.h
@@ -1,10 +1,10 @@
 /*
  * uFCoder.h
  *
- * library version: 4.0.29
+ * library version: 4.0.30
  *
  * Created on:  2009-01-14
- * Last edited: 2017-08-14
+ * Last edited: 2017-08-15
  *
  * Author: D-Logic
  */
