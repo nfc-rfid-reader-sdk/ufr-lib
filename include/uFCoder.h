8fab4fb325dbaf3c757c729526048b139b5d36d4
developer2 <support@d-logic.rs>
2018-02-13 15:12:58 +0100

    Version 4.3.6. BUG FIX ValueBlockWrite_PKHnd and ValueBlockInSectorWrite_PKHnd

--- include/uFCoder.h
+++ include/uFCoder.h
@@ -1,10 +1,10 @@
 /*
  * uFCoder.h
  *
- * library version: 4.3.5
+ * library version: 4.3.6
  *
  * Created on:  2009-01-14
- * Last edited: 2017-11-06
+ * Last edited: 2018-02-13
  *
  * Author: D-Logic
  */
