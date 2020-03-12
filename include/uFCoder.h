ba780861d4d771a5ff0f9769e05b71bd7dbd2262
developer34 <support@d-logic.rs>
2020-03-12 11:29:52 +0100

    Version 5.0.35 - UDP/TCP communication improved

--- include/uFCoder.h
+++ include/uFCoder.h
@@ -1,10 +1,10 @@
 /*
  * uFCoder.h
  *
- * library version: 5.0.34
+ * library version: 5.0.35
  *
  * Created on:  2009-01-14
- * Last edited: 2020-02-26
+ * Last edited: 2020-03-12
  *
  * Author: D-Logic
  */
